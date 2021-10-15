ddf7a943458a0e451715b37ed27baf856588b430
dmitry-bykovski <dmitry.bykovski.work@gmail.com>
2021-10-15 19:10:27 +0300

    update code

--- src/index.php
+++ src/index.php
@@ -5,21 +5,23 @@ namespace Dmitry\MyTasks;
 require __DIR__ . '/../vendor/autoload.php';
 
 use Dmitry\MyTasks\King;
-use Dmitry\MyTasks\Qeen;
+use Dmitry\MyTasks\Queen;
 
 if ($_POST['king'])
 {
-    $king = new King($_POST['king']['x'], $_POST['king']['y']);
+    $kingCoordinates = $_POST['king'];
+    $king = new King($kingCoordinates['x'], $kingCoordinates['y']);
     if ($king->move())
     {
         echo json_encode(['result' => 'success']);
     }
 }
 
-if ($_POST['qeen'])
+if ($_POST['queen'])
 {
-    $qeen = new Qeen($_POST['qeen']['x'], $_POST['qeen']['y']);
-    if ($qeen->move())
+    $queenCoordinates = $_POST['queen'];
+    $queen = new Queen($queenCoordinates['x'], $queenCoordinates['y']);
+    if ($queen->move())
     {
         echo json_encode(['result' => 'success']);
     }

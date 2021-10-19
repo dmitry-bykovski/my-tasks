f7807c37b1c56b4e4f26a9833317c2be8411b6eb
dmitry-bykovski <dmitry.bykovski.work@gmail.com>
2021-10-20 01:00:17 +0300

    fix psr

--- src/index.php
+++ src/index.php
@@ -7,20 +7,19 @@ require __DIR__ . '/../vendor/autoload.php';
 use Dmitry\MyTasks\King;
 use Dmitry\MyTasks\Queen;
 
-if ($_POST['king'])
-{
+if ($_POST['king']){
     $kingCoordinates = $_POST['king'];
     $king = new King($kingCoordinates['x'], $kingCoordinates['y']);
-    if ($king->move())
-    {
+    
+    if ($king->move()){
         echo json_encode(['coordinates' => $kingCoordinates]);
     }
 }
 
-if ($_POST['queen'])
-{
+if ($_POST['queen']){
     $queenCoordinates = $_POST['queen'];
     $queen = new Queen($queenCoordinates['x'], $queenCoordinates['y']);
+    
     if ($queen->move())
     {
         echo json_encode(['coordinates' => $queenCoordinates]);
